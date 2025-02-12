b13e86116ba1c2699cbd1e34f50690932c02f268
Florence Jay Munar <florencejaymunar@proton.me>
2025-02-12 12:36:16 +0800

    Changing JSON because im wrong

--- api.js
+++ api.js
@@ -69,7 +69,7 @@ if (audioPlayer && audioPlayer.dataset.src) {
         e.paused ? setPlayStatus() : setPauseStatus()
     }), !1)
 }
-const boxplay = "https://radio.ltg.network/api/nowplaying_static/main.json";
+const boxplay = "https://radio.ltg.network/api/nowplaying_static/radyongsira.json";
 
 function playerInit() {
     fetch(boxplay).then((e => e.json())).then((e => {
